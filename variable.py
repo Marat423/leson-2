ddfa83692eaa26f27cf547b7c33d2ed1a7de9013
Marat423 <msaltysh@gmail.com>
2024-09-07 14:51:58 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+quantity = 12 # Количество выполненых работ
+time = 1.5 # Количество затраченых часов
+well = 'Python'# курс
+
+time_for_task = time / quantity
+
+print('Курс:', well, ', всего задач:', quantity,', затраченно часов:', time, ', среднее время выполнения',time_for_task, 'часа.' )
